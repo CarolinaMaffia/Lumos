3924fb3218da9e7596ac37c56cdcca8396baf86b
Carolina Maffia <carolinamaffia.m@gmail.com>
2020-07-27 15:03:54 -0300

    Added react icons library

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Home.scss';
 import homeImg from '../../utils/svg/home.svg';
+import { RiQuillPenLine } from 'react-icons/ri';
 
 const Home = () => {
     return <div className="home-container">
             <div className="header">
                 <div className="logo-container">
-                    <p>
-                        Lumos
-                    </p>
+                    <h2>
+                         <RiQuillPenLine />
+                    </h2>
                 </div>
             </div>
             <div className="welcome-container">
