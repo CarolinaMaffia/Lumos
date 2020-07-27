bba5e63e048e0f21991046c450246531b89d2327
Carolina Maffia <carolinamaffia.m@gmail.com>
2020-07-27 17:47:56 -0300

    Basic home screen created

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import './Home.scss';
 import homeImg from '../../utils/svg/home.svg';
-import { RiQuillPenLine } from 'react-icons/ri';
+import quill from '../../utils/svg/quill.svg';
 
 const Home = () => {
     return <div className="home-container">
             <div className="header">
                 <div className="logo-container">
-                    <h2>
-                         <RiQuillPenLine />
-                    </h2>
+                    <p> Esto sera un logo </p>
+                    <img src={quill} alt="Quill logo"  className="logo"/>
                 </div>
             </div>
             <div className="welcome-container">
+                <div className="welcome-text">
+                    <h1>Lorem ipsum dolor sit amet </h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                </div>
                 <img src={homeImg} alt='Welcome to Lumos' className="home-img" />
+            </div>
+
+            <div className="start-container">
+                <button>
+                    Get started
+                </button>
             </div>
     </div>
 };
