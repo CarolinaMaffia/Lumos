4d5a861265d8f08814a3a04f408c4450964e2c78
Carolina Maffia <carolinamaffia.m@gmail.com>
2020-07-31 14:01:26 -0300

    Added basic funcionality of header input file

--- src/components/postHeader/PostHeader.js
+++ src/components/postHeader/PostHeader.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import './PostHeader.scss';
+import React, { useState } from 'react';import './PostHeader.scss';
 
 const PostHeader = () => {
+    const [showFile, setShowFile] = useState(0);
+
+    const handleChange = (event) => {
+        setShowFile(URL.createObjectURL(event.target.files[0]));
+    }
     return (
         <div className="post-header">
-            <input type="file" className="input-file" accept="image/gif, image/jpeg, image/png" />
+            <input type="file" className="input-file" onChange={handleChange} accept="image/gif, image/jpeg, image/png" />
+            <img src={showFile} alt="Header of the note" />
         </div>
     )
 }
